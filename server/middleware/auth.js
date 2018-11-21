42f791d91041e3724b6f5cb99c4ba08b2d8eef05
mahmoud hassan <mahmoudhassan@momuzio.homenet.telecomitalia.it>
2018-11-21 03:05:43 +0100

    v 1.0.0

---
+++ server/middleware/auth.js
@@ -0,0 +1,37 @@
+require('dotenv').load();
+var jwt = require('jsonwebtoken');
+
+
+
+exports.loginRequired = function (req, res, next ) {
+    try{
+        const token = req.headers.authorization.split(" ")[1];
+        jwt.verify(token, process.env.SECRET_KEY, function(err, decoded){
+            if(decoded) {
+                next();
+
+            }else{
+                return next({status:401, message: "please log in first "});
+            }
+        } )
+
+    }catch(e){
+        return next({status:401, message: "please log in first "});
+
+    }
+};
+exports.ensureCorrectUser = function(req, res ,next){
+    try{
+        const token = req.headers.authorization.split(" ")[1];
+        jwt.verify(token, process.env.SECRET_KEY, function(err, decoded){
+            if (decoded && decoded.id === req.params.id ){
+                return next();
+
+            }else{
+                return next({status:401, message:"UnAuthorized"});
+            }
+        });
+    }catch(e){
+        return next({status:401, message:"UnAuthorized"});
+    }
+};
---
+++ server/models/index.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+// mongo connect code 
+
+
+
+mongoose.set("debug", true);
+mongoose.Promise = Promise;
+mongoose.connect("mongodb://mido:mido2018@ds261253.mlab.com:61253/warbler",{
+    keepAlive: true,
+    useNewUrlParser: true 
+
+    
+});
+module.exports.User = require('./user');
+module.exports.Message = require('./message');
